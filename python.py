9b036e89bca33c0da67cd9106bcfa443f46e0a5f
OneOverCosine <TheCandyDragon@gmail.com>
2021-03-25 13:45:11 +0000

    Updated python.py

--- python.py
+++ python.py
@@ -7,15 +7,20 @@ class Python(Snake):
         self.large = True
         self.two_lungs = True
 
+        self.venom = False # polymorphism for an attribute
+
     def climb(self):
         return "Hello down there!"
 
     def constrict(self):
         return "I prefer the term 'hug'..."
 
+    def seek_warmth(self):
+        return "I climb trees, I think, to get warm..."
+
 monty = Python()
 
 print(monty.breathe()) # from Animal
-print(monty.seek_warmth()) # from Reptile
+print(monty.seek_warmth()) # from Reptile (now from self - polymorphism)
 print(monty.use_tongue_to_smell()) # from Snake
 print(monty.climb()) # own method
